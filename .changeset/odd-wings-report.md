172b510de197c5cf158d3b1c093a810badacc94b
xiongjie <jiexiong@futunn.com>
2025-08-13 14:07:09 +0800

    docs(changeset): 使用 exports 导出 use-fake-time，该函数在 storybook 开发环境下会报错，希望使用时明确的从另一个路径导入

---
+++ .changeset/odd-wings-report.md
@@ -0,0 +1,5 @@
+---
+'@cexoso/test-utils': patch
+---
+
+使用 exports 导出 use-fake-time，该函数在 storybook 开发环境下会报错，希望使用时明确的从另一个路径导入
--- packages/test-utils/src/index.ts
+++ packages/test-utils/src/index.ts
@@ -1,3 +1,2 @@
 export { getOrCreateStub } from './get-or-create-stub'
 export { delay } from './delay'
-export { useFakeTime } from './use-fake-time'
