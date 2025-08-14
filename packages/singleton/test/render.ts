401fde8b0e925afc7bbbc9d368a4076767f7da91
xiongjie <jiexiong@futunn.com>
2025-08-14 10:47:37 +0800

    chore: 卸载 div 时应该同时也把 app unmount 掉

--- packages/singleton/test/render.ts
+++ packages/singleton/test/render.ts
@@ -1,12 +1,32 @@
-import { Component, createApp } from 'vue'
-import { createContainer } from './create-container'
+import { Component, createApp, App } from 'vue'
 import { within } from '@testing-library/dom'
 import { createContext } from '../src/context'
+
+let div: HTMLDivElement | undefined
+const createContainer = () => {
+  if (div !== undefined) {
+    clean()
+  }
+  div = document.createElement('div')
+  return div
+}
+
+const clean = () => {
+  if (div) {
+    const parent = div.parentElement
+    parent?.removeChild(div)
+    const app = wm.get(div)
+    app?.unmount()
+  }
+}
+
+const wm = new WeakMap<HTMLDivElement, App<Element>>()
 
 export const renderComponent = (component: Component) => {
   const app = createApp(component)
   const container = createContainer()
   app.use(createContext()).mount(container)
+  wm.set(container, app)
   return {
     ...within(container),
     app,
