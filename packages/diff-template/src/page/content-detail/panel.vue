315166c59d09be712d97a43a79dcebf324d7f3d3
xiongjie <jiexiong@futunn.com>
2025-08-23 14:59:10 +0800

    docs(changeset): 去掉没有实现的功能，这个功能在我看来不重要，懒得实现了

--- packages/diff-template/src/page/content-detail/panel.vue
+++ packages/diff-template/src/page/content-detail/panel.vue
@@ -18,21 +18,6 @@
       </div>
 
       <CoveragePanel :coverage-data="coverageData" />
-
-      <p class="text-gray-600 mb-4">
-        Press <em class="font-semibold">n</em> or <em class="font-semibold">j</em> to go to the next uncovered
-        block, <em class="font-semibold">b</em>, <em class="font-semibold">p</em> or
-        <em class="font-semibold">k</em> for the previous block.
-      </p>
-
-      <div class="mb-4">
-        <label for="fileSearch" class="block text-gray-700 mb-2">Filter:</label>
-        <input
-          type="search"
-          id="fileSearch"
-          class="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
-      </div>
     </div>
   </div>
 </template>
