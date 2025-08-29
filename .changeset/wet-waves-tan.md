43af029b5a035cf3ac195885dddd2d45c274af02
xiongjie <jiexiong@futunn.com>
2025-08-29 19:09:48 +0800

    docs(changeset): 下掉不应该是这个库提供的 withResolver 方法

---
+++ .changeset/wet-waves-tan.md
@@ -0,0 +1,5 @@
+---
+'@cexoso/vue-singleton': patch
+---
+
+下掉不应该是这个库提供的 withResolver 方法
--- packages/vue-singleton/src/index.ts
+++ packages/vue-singleton/src/index.ts
@@ -1,5 +1,3 @@
 export { define } from './define-singleton'
 export { createContext, ContextKey } from './context'
-export { withResolvers } from './with-resolves'
-
 export { defineResource } from './define-resource'
